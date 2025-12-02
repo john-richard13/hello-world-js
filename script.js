65d9975d58fab58d1b4c82a55ecc1e03104178ae
Johnnie Richard Pereira <johnnielavras@gmail.com>
2025-12-02 14:57:50 -0300

    Refactor alerts and prompts for user input

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ alert('Seja bem vindo ao nosso site!');
 alert('Por favor, preencha todos os campos obrigatórios.');
 // Declarar uma variável para armazenar o nome do usuário
 let nome = 'Lua';
+
 // Solicita que o nome do usuario seja inserido
 let ide = prompt('Por favor, insira seu nome:');
+console.log('Nome inserido:', ide);
+
 // Saudação personalizada usando o nome inserido
-alert('Olá, ' + ide + '! Seja bem vindo ao nosso site!');
+alert(`Olá, ${ide}! Seja bem vindo ao nosso site.`);
 //Mensagem de erro para campos obrigatórios
 let mensagemDeErro = ('Erro! Preencha todos os campos obrigatórios.');
 // Agora exiba um alerta com o valor da variável
@@ -17,6 +20,7 @@ alert(mensagemDeErro);
 let idade = 25;
 // Solicita que o usuário indique sua idade
 let age = prompt('Qual é sua idade?');
+console.log('Idade inserida:', age);
 // Condicional para verificar se o usuário é maior de idade
 if (age >= 18) {
     alert('Você pode tirar a habilitação de motorista.');
@@ -26,12 +30,19 @@ if (age >= 18) {
 
 // Guardando o número secreto em uma variável
 let numeroSecreto = 42;
+console.log('Número secreto:', numeroSecreto);
 // Solicita que o usuário insira um palpite para o número secreto
 let chute = prompt('Tente adivinhar o número secreto (entre 1 e 100):');
 // Condicional para verificar se o palpite está correto
-if (numeroSecreto == chute) {
-    alert('Parabéns! Você acertou o número secreto (42)!');
-} else {alert('Infelizmente, você errou. Tente novamente!');}
+while (true) {
+    if (chute < numeroSecreto) {
+        chute = prompt('Muito baixo! Tente novamente:');}
+    else if (chute > numeroSecreto) {
+        chute = prompt('Muito alto! Tente novamente:');}
+    else {
+        break;}
+    }
+alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}!`);
 
 
 // Variável de vendas
