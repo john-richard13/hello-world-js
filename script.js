3fd174f8c2e00f3cc82b795d1fa6b13463a0aea6
Johnnie Richard Pereira <johnnielavras@gmail.com>
2025-12-04 14:08:07 -0300

    Update guessing game and user input handling
    Refactor guessing game logic and improve user prompts

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 alert('Seja bem vindo ao nosso site!');
 // Necessidade de preencher todos os campos
 alert('Por favor, preencha todos os campos obrigatórios.');
-// Declarar uma variável para armazenar o nome do usuário
-let nome = 'Lua';
 
 // Solicita que o nome do usuario seja inserido
 let ide = prompt('Por favor, insira seu nome:');
@@ -16,8 +14,6 @@ let mensagemDeErro = ('Erro! Preencha todos os campos obrigatórios.');
 // Agora exiba um alerta com o valor da variável
 alert(mensagemDeErro);
 
-// Idade do usuário
-let idade = 25;
 // Solicita que o usuário indique sua idade
 let age = prompt('Qual é sua idade?');
 console.log('Idade inserida:', age);
@@ -29,29 +25,31 @@ if (age >= 18) {
 }
 
 // Guardando o número secreto em uma variável
-let numeroSecreto = 42;
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto)
+let chute;
+let tentativas = 1;
 console.log('Número secreto:', numeroSecreto);
 // Solicita que o usuário insira um palpite para o número secreto
 let chute = prompt('Tente adivinhar o número secreto (entre 1 e 100):');
 // Condicional para verificar se o palpite está correto
-while (true) {
-    if (chute < numeroSecreto) {
-        chute = prompt('Muito baixo! Tente novamente:');}
-    else if (chute > numeroSecreto) {
-        chute = prompt('Muito alto! Tente novamente:');}
-    else {
-        break;}
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        // tentativas = tentativas + 1
+        tentativas++
     }
-alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}!`);
-
-
-// Variável de vendas
-let vendas = prompt('Insira o valor das vendas que deseja realizar:');
-// Variável de saldo disponível
-let saldoDisponivel = 1000;
-// Condicional para verificar se as vendas são compátiveis com o saldo disponível
-if (vendas <= saldoDisponivel) {
-    alert('Venda realizada com sucesso!');
-} else {
-    alert('Saldo insuficiente para realizar a venda.');
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' //Este comando significa que: se tentativas for maior que 1, a variável palavraTentativa recebe 'tentativas'.
+Caso contrário, recebe 'tentativa'.”
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}!`);
